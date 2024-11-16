3e9860c945d36f037b4f1e0e3fec7d8f7adbaf18
edenma5 <edenmaimon8@gmail.com>
2024-11-16 22:03:45 +0200

    fix photos gallery

--- pages/api/getTokens.js
+++ pages/api/getTokens.js
@@ -5,20 +5,12 @@ import * as http from 'http'; //ES 6
 import * as url from "url";
 import * as querystring from "querystring";
 
-// Replace with your credentials
-const CLIENT_ID = '207819093641-ot03in5gj1v2i6np5rov4o5jq5qdetl5.apps.googleusercontent.com';
-const CLIENT_SECRET = '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';
-const REDIRECT_URI = 'http://localhost:3000/oauth2callback';
-
-// Scope for read-only access to Google Photos
-const SCOPE = 'https://www.googleapis.com/auth/photoslibrary.readonly';
-
 // Step 1: Get authorization code
 const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?${querystring.stringify({
-    client_id: CLIENT_ID,
-    redirect_uri: REDIRECT_URI,
+    client_id: process.env.CLIENT_ID,
+    redirect_uri: process.env.REDIRECT_URI,
     response_type: 'code',
-    scope: SCOPE,
+    scope: process.env.SCOPE,
     access_type: 'offline',
     prompt: 'consent',
 })}`;
@@ -40,9 +32,9 @@ const server = http.createServer(async (req, res) => {
             const tokenResponse = await axios.post('https://oauth2.googleapis.com/token', null, {
                 params: {
                     code: code,
-                    client_id: CLIENT_ID,
-                    client_secret: CLIENT_SECRET,
-                    redirect_uri: REDIRECT_URI,
+                    client_id: process.env.CLIENT_ID,
+                    client_secret: process.env.CLIENT_SECRET,
+                    redirect_uri: process.env.REDIRECT_URI,
                     grant_type: 'authorization_code',
                 },
             });
